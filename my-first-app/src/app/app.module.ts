6cca7919bb4fab9ef273cce21e6772083aade5e8
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-19 14:48:07 -0300

    import server.component to app.module

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,

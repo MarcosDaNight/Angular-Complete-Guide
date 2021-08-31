c85ffc29fa9784648410448dc66ef58000ed8bed
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-31 16:12:20 -0300

    Adding Navigation with Event Biding and ngIf

--- project-start/ng4-complete-guide/src/app/app.component.ts
+++ project-start/ng4-complete-guide/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng4-complete-guide';
+  title = 'Recipes App';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }

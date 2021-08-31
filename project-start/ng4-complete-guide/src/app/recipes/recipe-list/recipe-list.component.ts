bb1e07e1b5523ab54ea39a9b03e5798cf618fcda
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-31 20:19:15 -0300

    Passing Data with Event and Property Binding

--- project-start/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-start/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 
@@ -8,15 +8,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This a simply a test',
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F20%2F4035780.jpg&q=85')
+    new Recipe('A Test Recipe', 'This a simply a test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F20%2F4035780.jpg&q=85'),
+    new Recipe('Another Test Recipe', 'This a another simply a test',
+      'https://www.simplyrecipes.com/thmb/SDsQm323X7tk4wRZ0sXtkL-jvuM=/1600x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }

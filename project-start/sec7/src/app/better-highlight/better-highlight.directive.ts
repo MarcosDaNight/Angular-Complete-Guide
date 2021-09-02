daeadcd88fc8ec7d6f27981e2af1593417f8f4fc
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-09-02 16:23:24 -0300

    Using HostBiding to Bind to Host Properties

--- project-start/sec7/src/app/better-highlight/better-highlight.directive.ts
+++ project-start/sec7/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,7 +8,15 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private render: Renderer2) { }
 
   ngOnInit() {
+    //this.render.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false);
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.render.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.render.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
 

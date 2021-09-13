f1cbaa6532f57b07c56fa2a4537f4e3c13c7d298
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-09-13 18:15:23 -0300

    Adding Ingredients to Recipe

--- project-start/prj-directives-final/src/app/recipes/recipe.service.ts
+++ project-start/prj-directives-final/src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
@@ -5,10 +6,29 @@ export class RecipeService {
    recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-      new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-      new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+      new Recipe(
+         'Tasty Schitzel', 
+         'A special Schitzel, this is recipe remeber my old childhood', 
+         'https://media.istockphoto.com/photos/tasty-schnitzel-with-cucumber-salad-picture-id944084736?k=6&m=944084736&s=170667a&w=0&h=8OX1w_uzoPHSPTpfRxv05KW1ai7-xdkzkZeGTo9Gmws=',
+         [
+            new Ingredient('Chicken Meat', 1),
+            new Ingredient('Lemon', 4),
+            new Ingredient('Breadcrumbs', 6),
+            new Ingredient('Soy oil', 2)
+         ]),
+      new Recipe(
+         'Big Burguer', 
+         'This is a BIG SPECIAL AND DELICIUS BURGUER!', 
+         'https://digaolanches.com.br/site/wp-content/uploads/2019/09/big-burguer.png',
+         [
+            new Ingredient('Meat', 1),
+            new Ingredient('Bacon', 5),
+            new Ingredient ('Lettuce', 3),
+            new Ingredient('Onion', 2),
+            new Ingredient('French Fries', 20),
+            new Ingredient('Bread', 3)
+         ])
     ];
-
 
    getRecipes() {
       return this.recipes.slice();

4341f2286aa25375f48a7b575313461485838832
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-09-24 00:38:44 -0300

    Resolving Dynamic Data with the resolve Guard

--- project-start/routing-start/src/app/app-routing.module.ts
+++ project-start/routing-start/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ServerResolver } from './servers/server/server-resolver.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { AuthGuard } from './auth-guard.service';
 import { NgModule } from "@angular/core";
@@ -21,7 +22,7 @@ const appRoutes: Routes = [
       //canActivate: [AuthGuard], 
       canActivateChild: [AuthGuard], 
       component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ] },
     //{ path: 'not-found', component: PageNotFoundComponent },
--- project-start/routing-start/src/app/app.module.ts
+++ project-start/routing-start/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ServerResolver } from './servers/server/server-resolver.service';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth-service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -37,7 +38,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
